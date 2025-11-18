845fecb6734099cb9b69dc0b6d27131cdabad781
Reshuyadav1418 <reshuyadav1409@gmail.com>
2025-11-18 12:41:35 +0530

    change baseUrl

--- frontened/src/utils/config.js
+++ frontened/src/utils/config.js
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http://localhost:4000/api/v1';
+// export const BASE_URL = 'http://localhost:4000/api/v1';
+
+
+export const BASE_URL = 'https://tour-and-travel-1-edw6.onrender.com/api/v1';
